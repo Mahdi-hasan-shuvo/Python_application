8e1abc99a99420b9865d2b84e31cad3c0809d2d7
MAHDI HASAN SHUVO <102132190+Mahdi-hasan-shuvo@users.noreply.github.com>
2024-09-16 01:12:35 +0600

    Create source.py

---
+++ day1/source.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+windos_apps=Tk()
+windos_apps.title("Windos Apps") # apps title 
+windos_apps.geometry("500x400") # apps size
+adding_text=Label(windos_apps,text="show text")# any text you want to show
+adding_text.grid(row=1,column=1)
+
+windos_apps.mainloop()
+
+
